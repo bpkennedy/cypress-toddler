8c5f464af80c4ad0f0723a5b09fc06f1c13b2edd
Brian Kennedy <bpkennedy@gmail.com>
2019-11-24 21:14:24 -0600

    Add initial project files

---
+++ cypress/integration/mouse.spec.js
@@ -0,0 +1,28 @@
+describe('Mouse', () => {
+  beforeEach(() => {
+    cy.setup()
+  })
+
+  it('should track left click down/up and right click down/up', () => {
+    cy.randomMouseDown('left')
+    cy.assertMouseCounts([0,1,0,0,0])
+
+    cy.randomMouseUp('left')
+    cy.assertMouseCounts([0,1,1,0,0])
+
+    cy.randomMouseDown('right')
+    cy.assertMouseCounts([0,1,1,1,0])
+
+    cy.randomMouseUp('right')
+    cy.assertMouseCounts([0,1,1,1,1])
+  })
+  
+  it.only('should randomly move mouse by count', () => {
+    cy.singleMouseMove()
+    cy.assertMouseCounts([1,0,0,0,0])
+    
+    cy.randomMouseMove(11)
+    cy.assertMouseCounts([10,0,0,0,0])
+  })
+
+})
---
+++ src/index.js
@@ -0,0 +1,60 @@
+const width = 800
+const height = 800
+const mouseClickCode = { left: 1, right: 3 }
+
+function randomIntFromInterval(min, max) { // min and max included 
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+const pageX = () => {
+  return randomIntFromInterval(1, width)
+}
+
+const pageY = () => {
+  return randomIntFromInterval(1, height)
+}
+
+Cypress.Commands.add('setup', () => {
+  cy.viewport(width, height)
+  cy.visit('http://localhost:8080')
+})
+
+Cypress.Commands.add('randomMouseDown', (direction) => {
+  cy.get('body').trigger('mousedown',  pageX(), pageY(), { which: mouseClickCode[direction] })
+})
+
+Cypress.Commands.add('randomMouseUp', (direction) => {
+  cy.get('body').trigger('mouseup',  pageX(), pageY(), { which: mouseClickCode[direction] })
+})
+
+Cypress.Commands.add('mouseUp', (direction) => {
+  cy.get('body').trigger('mouseup',  { which: mouseClickCode[direction] })
+})
+
+Cypress.Commands.add('randomMouseMove', (count) => {
+  let X = pageX()
+  let Y = pageY()
+  cy.singleMouseMove(X, Y)
+  for (const m of Array(count).keys()) {
+    X += 1
+    Y += 1
+    cy.singleMouseMove(X, Y)
+  }
+})
+
+Cypress.Commands.add('singleMouseMove', (posX = pageX(), posY = pageY()) => {
+  cy.get('body').trigger('mousemove', posX, posY)
+  // cy.get('body').trigger('mousemove', posX + 1, posY + 1)
+})
+
+Cypress.Commands.add('assertMouseCounts', (mouseEvents) => {
+  cy.contains(`mousemove:${mouseEvents[0]}`)
+  cy.contains(`leftclickdown:${mouseEvents[1]}`)
+  cy.contains(`leftclickup:${mouseEvents[2]}`)
+  cy.contains(`rightclickdown:${mouseEvents[3]}`)
+  cy.contains(`rightclickup:${mouseEvents[4]}`)
+})
+
+Cypress.Commands.add('toddle', { prevSubject: 'element' }, (element, options = {}) => {
+  console.log('tested....')
+});
